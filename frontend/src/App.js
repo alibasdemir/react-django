4370e2ecd7afa5ac5527c1692ecbea7aee69aab5
FakirHerif <basdemir35@hotmail.com.tr>
2023-11-03 04:56:46 +0300

    not found page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import Signup from "./components/signup/Signup";
 import Login from "./components/login/Login";
 import Dashboard from "./components/dashboard/Dashboard";
+import NotFoundPage from "./components/NotFoundPage";
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
             <Route path="/login" element={<Login />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route exact path="/" element={<Home />} />
+            <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </BrowserRouter>
       </div>
---
+++ frontend/src/components/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NotFoundPage() {
+  return (
+    <div>
+    <h2>404 - Sayfa Bulunamadı</h2>
+    <p>Aradığınız sayfa maalesef bulunamadı.</p>
+  </div>
+  )
+}
+
+export default NotFoundPage
