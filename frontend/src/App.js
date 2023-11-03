bc92a38903f1dc83737c9a97c68dcabb83bc6535
FakirHerif <basdemir35@hotmail.com.tr>
2023-11-03 04:50:22 +0300

    added home, signup, login, and dashboard components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,31 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Signup from "./components/signup/Signup";
+import Login from "./components/login/Login";
+import Dashboard from "./components/dashboard/Dashboard";
 
 class App extends Component {
   render() {
-    return <h1>Hi!</h1>;
+    return (
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route exact path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    
+    
+
+
+
+
+    
+    );
   }
 }
 
