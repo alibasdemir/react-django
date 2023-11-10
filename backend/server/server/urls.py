ea60e4c4d52a833c20162f2ddba47aebbee8aecb
FakirHerif <basdemir35@hotmail.com.tr>
2023-11-10 08:08:11 +0300

    added media serving for DEBUG mode

--- backend/server/server/urls.py
+++ backend/server/server/urls.py
@@ -19,8 +19,14 @@ from django.urls import path
 
 from apps.accounts.urls import accounts_urlpatterns
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
 urlpatterns += accounts_urlpatterns
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
