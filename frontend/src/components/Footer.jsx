b3f12448ff4e69193ad46ac7c8cc0f2f0c466da6
Gokce Tasci <gokcetascii@gmail.com>
2023-11-20 02:25:24 +0300

    scrolltop and style added

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,12 +7,17 @@ import { BsSpotify } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
 
 function Footer() {
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" })
+  }
+
   return (
     <footer className="bg-[#080243] to-indigo-900 text-white py-6 bottom-0">
       <div className="flex justify-between mb-10 mx-24">
-        <div className="w-1/5 flex flex-col justify-center items-center pr-10">
-          
-          <Link to="/" className="text-3xl font-bold italic text-white mb-3 flex justify-center items-center">
+        <div className="w-1/5 flex flex-col justify-center items-center pr-10 mt-4">
+
+          <Link to="/" onClick={scrollToTop} className="text-3xl font-bold italic text-white mb-3 flex justify-center items-center no-underline">
             <span style={{ color: "#ff4500" }}>TICKET</span>
             <span style={{ color: "#a5b4fc", fontSize: "40px" }}>X</span>
           </Link>
@@ -24,32 +29,31 @@ function Footer() {
         <div className="w-1/7 pt-5">
           <h3 className="text-l font-bold mb-3 pb-2">Hakkımızda</h3>
           <ul>
-            <li><a href="#" className='text-slate-300 text-sm mb-1 hover:underline hover:text-purple-700'>Biz Kimiz</a></li>
-            <li><a href="#" className='text-slate-300 text-sm mb-1 hover:underline hover:text-purple-700'>Kurum Politikamız</a></li>
+            <li><a href="#" className='text-slate-300 text-sm mb-1 no-underline hover:underline hover:text-purple-700 '>Biz Kimiz</a></li>
+            <li><a href="#" className='text-slate-300 text-sm mb-1 no-underline hover:underline hover:text-purple-700'>Kurum Politikamız</a></li>
           </ul>
         </div>
 
         <div className="w-1/7 pt-5">
           <h3 className="text-l font-bold mb-3 pb-2">Bağlantılar</h3>
           <ul>
-            <li><a href="#" className='text-slate-300 text-sm mb-1 hover:underline hover:text-purple-700'>Anasayfa</a></li>
-            <li><a href="#" className='text-slate-300 text-sm mb-1 hover:underline hover:text-purple-700'>Etkinlikler</a></li>
-            <li><a href="#" className='text-slate-300 text-sm mb-1 hover:underline hover:text-purple-700'>Biletler</a></li>
-            <li><a href="#" className='text-slate-300 text-sm mb-1 hover:underline hover:text-purple-700'>Yardım/SSS</a></li>
+            <li><a href="#" className='text-slate-300 text-sm mb-1 no-underline hover:underline hover:text-purple-700'>Anasayfa</a></li>
+            <li><a href="#" className='text-slate-300 text-sm mb-1 no-underline hover:underline hover:text-purple-700'>Etkinlikler</a></li>
+            <li><a href="#" className='text-slate-300 text-sm mb-1 no-underline hover:underline hover:text-purple-700'>Biletler</a></li>
+            <li><a href="#" className='text-slate-300 text-sm mb-1 no-underline hover:underline hover:text-purple-700'>Yardım/SSS</a></li>
           </ul>
         </div>
-
-        <div className="w-1/7 pt-5">
+        <div className="w-1/7 pt-5">  {/* burada p etiketinde sanırım bootstrap default p mb değeri var ondan farklı gözüküyor. */}
           <h3 className="text-l font-bold mb-3 pb-2">İletişim</h3>
-          <address className="text-slate-300 text-sm mb-2">
+          <address className="text-slate-300 text-sm ">
             <p>Adres: 1234 Sokak No: 5</p>
             <p>Telefon: <a href="tel:(123) 456-7890" className="text-purple-500"> (123) 456-7890</a></p>
             <p>E-posta: <a href="mailto:info@example.com" className="text-purple-500">info@example.com</a></p>
           </address>
         </div>
 
-        <div className="w-1/5">
-          <div className='flex flex-row'>
+        <div className="w-1/5 ">
+          <div className='flex flex-row mt-10'>
             <h3 className="text-gray-200 mt-2 font-bold text-l mb-5">Bizi Takip Edin</h3>
           </div>
           <div className="flex flex-row space-x-4">
@@ -98,7 +102,10 @@ function Footer() {
         <div className='ml-5'></div>
         <p className='hover:underline hover:text-purple-700' style={{ cursor: 'pointer' }}>Gizlilik politikası</p>
         <div className='ml-5'></div>
-        <p className=' ml-auto'>©Copyright 2023 TICKET <b>X</b></p>
+        <p className=' ml-auto'>©Copyright 2023 <Link to="/" onClick={scrollToTop} className="text-sm font-bold italic text-white mb-3 no-underline">
+          <span style={{ color: "#ff4500" }}>TICKET</span>
+          <span style={{ color: "#a5b4fc", fontSize: "16px" }}>X</span>
+        </Link></p>
       </div>
     </footer>
   );
