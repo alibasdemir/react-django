36248dc46c359342682befb41d5c43d1a1527fd9
FakirHerif <basdemir35@hotmail.com.tr>
2023-11-24 03:58:33 +0300

    date settings adjusted on finishedevents page, clickability of events was removed

--- frontend/src/components/Events/FinishedEventsPage.jsx
+++ frontend/src/components/Events/FinishedEventsPage.jsx
@@ -8,11 +8,9 @@ import { MdDateRange } from "react-icons/md";
 import { BiSolidHome } from "react-icons/bi";
 import { RiArrowRightSLine } from "react-icons/ri";
 
-
-
 const FinishedEventsPage = () => {
   const [events, setEvents] = useState([]);
-  const targetDate = new Date('2023-12-25'); // Hedef tarih: 25 Aralık 2023
+  const currentDate = new Date(); // Güncel tarih
   const formatDate = (dateString) => {
     const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
     return new Date(dateString).toLocaleDateString('tr-TR', options);
@@ -21,11 +19,10 @@ const FinishedEventsPage = () => {
   useEffect(() => {
     axios.get('http://localhost:8000/events/')
       .then(response => {
-        const currentDate = new Date();
         const filteredEvents = response.data.filter(event => {
           const endDate = new Date(event.end_date);
-          // Etkinlik bitiş tarihi, hedef tarihten küçük ve etkinlik aktifse
-          return endDate < currentDate && endDate < targetDate;
+          // Etkinlik bitiş tarihi, bugünkü tarihten önceyse ve etkinlik aktifse
+          return endDate < currentDate;
         });
 
         filteredEvents.sort((a, b) => a.id - b.id);
@@ -35,7 +32,7 @@ const FinishedEventsPage = () => {
       .catch(error => {
         console.error('Veri çekme hatası:', error);
       });
-  }, []);
+  }, [currentDate]);
 
   return (
     <div>
@@ -44,11 +41,15 @@ const FinishedEventsPage = () => {
       <div className='bg-gray-100 px-40 py-20'>
         <div className='bg-white px-20 py-10 rounded-2xl' style={{ boxShadow: '0 4px 8px #cbd5e1' }}>
           <div className='pb-8 flex flex-row items-center'>
-          <Link to="/" className=' no-underline  hover:scale-105 '>
+            <Link to="/" className=' no-underline  hover:scale-105 '>
               <BiSolidHome className='text-2xl mr-1 mb-2 text-rose-500 hover:text-rose-500' />
             </Link>
-            <RiArrowRightSLine className='text-2xl  text-indigo-500' />
-            <span className='text-indigo-500  no-underline'>Etkinlik</span>
+            <Link to="/categories/all" style={{ textDecoration: 'none' }}>
+              <div className="flex items-center">
+                <RiArrowRightSLine className='text-2xl text-indigo-500' />
+                <p className='text-indigo-500 no-underline'>Etkinlik</p>
+              </div>
+            </Link>
             <RiArrowRightSLine className='text-2xl  text-indigo-500' /><span className='text-rose-500 text-lg font-bold '>Süresi Geçen Etkinlikler</span>
           </div>
 
@@ -57,7 +58,7 @@ const FinishedEventsPage = () => {
               {events.map(event => (
                 <div key={event.id} className='mb-4 flex flex-row border border-gray-700 p-3 rounded-2xl space-x-4 justify-between ' style={{ boxShadow: '0 4px 8px #cbd5e1' }}>
                   <div className='flex flex-row space-x-8'>
-                    <Link to={`/event/${event.id}`}>
+                    
                       {event.eventImages && event.eventImages.length > 0 && (
                         <div
                           style={{
@@ -78,27 +79,27 @@ const FinishedEventsPage = () => {
                           />
                         </div>
                       )}
-                    </Link>  
+                    
                     <div>
                       <span className='text-lg text-indigo-700 font-bold'>{event.name}</span>
-                      <div className=" mt-4 mr-2 bg-red-500 p-2 text-white font-bold rounded-md text-sm hover:scale-110 cursor-pointer hover:bg-red-500 transition-colors ease-in-out duration-500 animate-pulse ">
-                <i>Üzgünüz, bu etkinliğin tarihi geçti!</i>
-              </div>
+                      {new Date(event.end_date) < new Date() && (
+                        <div className=" mt-4 mr-2 bg-red-500 p-2 text-white font-bold rounded-md text-sm hover:scale-110 cursor-pointer hover:bg-red-500 transition-colors ease-in-out duration-500 animate-pulse " style={{ cursor: 'default' }}>
+                          <i>Üzgünüz, bu etkinliğin tarihi geçti!</i>
+                        </div>
+                      )}
                     </div>
                   </div>
                   <div>
                     <div className='flex flex-row  space-x-8'>
-
-                    <div className='flex flex-col p-3 space-y-2'>
-  <div className='flex flex-row justify-center px-2 items-center'>
-    <FaMapMarkerAlt className='text-rose-500 mr-1' />
-    <span className='font-bold'>{event.owner}</span>
-  </div>
-  <div className='flex flex-row justify-center px-2 items-center'>
-    <p className='text-gray-700 text-sm'>{event.location}</p>
-  </div>
-</div>
-
+                      <div className='flex flex-col p-3 space-y-2'>
+                        <div className='flex flex-row justify-center px-2 items-center'>
+                          <FaMapMarkerAlt className='text-rose-500 mr-1' />
+                          <span className='font-bold'>{event.owner}</span>
+                        </div>
+                        <div className='flex flex-row justify-center px-2 items-center'>
+                          <p className='text-gray-700 text-sm'>{event.location}</p>
+                        </div>
+                      </div>
                       <div className='flex flex-row justify-center  pt-3'>
                         <MdDateRange className="text-rose-600 mr-1" />
                         <span className='text-sm'>Bitiş Tarihi: {formatDate(event.end_date)}</span>
@@ -106,12 +107,8 @@ const FinishedEventsPage = () => {
                       </div>
                     </div>
                   </div>
-
                 </div>
               ))}
-              {/* <div className=''>
-                    <p>{event.description}</p>
-                  </div> */}
             </div>
           </div>
         </div>
