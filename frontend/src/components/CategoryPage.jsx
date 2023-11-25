382ae20d55f8c79c2cd9925d265e02404f4a31d7
Gokce Tasci <gokcetascii@gmail.com>
2023-11-26 00:39:04 +0300

    responsive fix added

--- frontend/src/components/CategoryPage.jsx
+++ frontend/src/components/CategoryPage.jsx
@@ -133,19 +133,20 @@ function CategoryPage() {
       <div className='p-4 sm:p-6 lg:p-10 bg-gray-100'>
         <div className='ml-[1.5rem] mr-[1.5rem]'>
           <div className='mb-6 mt-3 lg:mb-12'>
-            <div className='flex flex-row justify-between items center border-b border-indigo-500 mb-1 px-4 '>
-
-              <h2 className="text-start font-bold text-blue-900">
-                {categoryTitle}
+            <div className='flex flex-row justify-between items-center border-b border-indigo-500 mb-1 px-4 '>
+              <h2 className="text-start font-bold text-blue-900 text-base sm:text-2xl">                {categoryTitle}
               </h2>
-              <div className='flex flex-row text-blue-900 pt-3'>
+              <div className='flex flex-row text-blue-900 lg:pt-3 '>
                 <Link to={"/"}><BiSolidHome className='text-xl mr-1 text-blue-900 hover:scale-105' /></Link>
-                
-                <Link to="/" className='text-blue-900 no-underline mr-2 hover:text-blue-600'>Anasayfa</Link>
-                <span style={{ width: "5px", height: "5px", borderRadius: "50%" }} className='bg-blue-900 mt-2.5'></span>
-                <p className='ml-2'>{categoryTitle}</p>
+
+                <Link to="/" className='text-blue-900 no-underline mr-2  hover:text-blue-600 hidden sm:inline'>Anasayfa</Link>
+
+                <span style={{ width: "5px", height: "5px", borderRadius: "50%" }} className='bg-blue-900 mt-2.5 hidden sm:inline'></span>
+
+                <p className='ml-2 hidden sm:inline'>{categoryTitle}</p>
               </div>
             </div>
+
           </div>
 
           <div className='flex flex-col justify-between '>
@@ -164,70 +165,73 @@ function CategoryPage() {
               />
             )}
 
-            <div className='w-fit h-fit mt-4 my-10 mx-auto'>
-              <div className='bg-gray-200 p-4 rounded-2xl mx-20' >
-                <div className='filter-div flex flex-row bg-white/75 rounded-2xl shadow-lg shadow-indigo-500/50 '>
-                  <div className='border-r border-indigo-300'>
-                  <div className='owner flex flex-col  px-6 m-2 items-center py-3'>
-                    <h5 className="text-blue-600 flex items-center text-lg mb-3 ">Tarih</h5>
-                    <div className='flex flex-col gap-2 items-center'>
-                     <div className='flex flex-row gap-3'>
-                     <p className="text-sm flex flex-col mb-2 pb-1 items-center border-b border-indigo-300 mr-3 gap-2">
-                        <b>Başlangıç Tarihi: </b><input type="date" onChange={handleDate} name="start" value={date.start} />
-                      </p>
-                      <p className="text-sm flex flex-col mb-2 pb-1 items-center border-b border-indigo-300 ml-3 gap-2">
-                        <b>Bitiş Tarihi:</b> <input type="date" onChange={handleDate} name="end" value={date.end} />
-                      </p>
-                     </div>
-                      <div className='flex flex-col mt-3'>
-                        <button className='cursor-pointer flex flex-row justify-center items-center bg-blue-500 text-white rounded-md p-2 mb-3 hover:bg-indigo-700 transition-colors ease-in-out duration-300 hover:scale-105 text-center' id='search' onClick={handleFilter}>
-                          ARA
-                        </button>
-                        <div className='clear-btn'>
-                          <button
-                            className='bg-red-500 text-white rounded-md text-sm w-40 h-10 p-2  text-center flex justify-center items-center cursor-pointer hover:bg-red-700 transition-colors ease-in-out duration-300 hover:scale-105'
-                            onClick={handleClear}
-                          >
-                            <MdClear className=" text-lg mr-1" /> Filtreleri Temizle
+            <div className=' w-full lg:w-fit h-fit mt-4 my-10 mx-auto'>
+              <div className='bg-gray-200 p-4 rounded-2xl lg:mx-20' >
+                <div className='filter-div flex flex-col lg:flex-row bg-white/75 rounded-2xl shadow-lg shadow-indigo-500/50 '>
+                  <div className='lg:border-r lg:border-indigo-300 '>
+                    <div className='owner flex flex-col  px-6 m-2 items-center py-3 '>
+                      <h5 className="text-blue-600 flex items-center text-lg mb-3 ">Tarih</h5>
+                      <div className='flex flex-col gap-2 items-center'>
+                        <div className='flex flex-col md:flex-row lg:flex-row gap-3'>
+                          <p className="text-sm flex flex-col mb-2 pb-1 items-center border-b border-indigo-300 md:border-r lg:border-r-0 lg:mb-0 lg:mr-3 gap-2">
+                            <b>Başlangıç Tarihi: </b><input type="date" onChange={handleDate} name="start" value={date.start} />
+                          </p>
+                          <p className="text-sm flex flex-col mb-2 pb-1 items-center border-b border-indigo-300 ml-3 md:mb-0 lg:mb-0 gap-2">
+                            <b>Bitiş Tarihi:</b> <input type="date" onChange={handleDate} name="end" value={date.end} />
+                          </p>
+                        </div>
+
+                        <div className='flex flex-col mt-3'>
+                          <button className='cursor-pointer flex flex-row justify-center items-center bg-blue-500 text-white rounded-md p-2 mb-3 hover:bg-indigo-700 transition-colors ease-in-out duration-300 hover:scale-105 text-center' id='search' onClick={handleFilter}>
+                            ARA
                           </button>
+                          <div className='clear-btn'>
+                            <button
+                              className='bg-red-500 text-white rounded-md text-sm w-40 h-10 p-2  text-center flex justify-center items-center cursor-pointer hover:bg-red-700 transition-colors ease-in-out duration-300 hover:scale-105'
+                              onClick={handleClear}
+                            >
+                              <MdClear className=" text-lg mr-1" /> Filtreleri Temizle
+                            </button>
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
+                  
+                  <div className='flex flex-col md:flex-row '>
+                    <div className='md:border-r flex md:border-indigo-300 mx-20 md:mx-0'>
+                    <div className='location flex flex-col my-3 md:mx-20 items-center '>
+                        <h5 className="text-blue-600 text-lg mb-2 border-b border-indigo-400">Konum</h5>
+                        <ul className='p-0 flex flex-col gap-1 list-disc'>
+                          {Object.entries(locationCount).map(([location, count]) => (
+                            <li
+                              className='cursor-pointer text-xs p-1'
+                              onClick={() => handleFilter('location', location)}
+                              key={location}
+                            >
+                              {location} ({count})
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    </div>
+
+                    <div className='owner flex flex-col my-3 md:mx-20 items-center '>
+                      <h5 className="text-blue-600 flex text-lg mb-2 border-b border-indigo-400">Mekan</h5>
+                      <ul className='p-0 flex flex-col  gap-1 list-disc'>
+                        {Object.entries(ownerCount).map(([owner, count]) => (
+                          <li
+                            className='cursor-pointer text-xs p-1'
+                            onClick={() => handleFilter('owner', owner)}
+                            key={owner}
+                          >
+                            {owner} ({count})
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
                   </div>
 
-               <div className='border-r flex  border-indigo-300 '>
-               <div className='location flex flex-col px-2 my-3 mx-20 items-center '>
-                    <h5 className="text-blue-600  text-lg mb-2 border-b border-indigo-400">Konum</h5>
-                    <ul className='p-0 flex flex-col gap-1 list-disc'>
-                      {Object.entries(locationCount).map(([location, count]) => (
-                        <li
-                          className='cursor-pointer  text-xs p-1'
-                          onClick={() => handleFilter('location', location)}
-                          key={location}
-                        >
-                          {location} ({count})
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-               </div>
-
-
-                  <div className='owner flex flex-col pr-2 pl-2 my-3 mx-20  items-center '>
-                    <h5 className="text-blue-600 flex  text-lg mb-2 border-b border-indigo-400">Mekan</h5>
-                    <ul className='p-0 flex flex-col gap-1 list-disc'>
-                      {Object.entries(ownerCount).map(([owner, count]) => (
-                        <li
-                          className='cursor-pointer  text-xs p-1'
-                          onClick={() => handleFilter('owner', owner)}
-                          key={owner}
-                        >
-                          {owner} ({count})
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
                 </div>
               </div>
             </div>
